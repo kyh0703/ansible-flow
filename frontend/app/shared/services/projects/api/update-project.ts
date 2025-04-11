afb7d3b99ef91435cdea3363907a909af8ab6f56
Kimyeonho <kyh0703@nate.com>
2025-04-11 11:30:08 +0900

    Feat: [FRONT] Patch 변경

--- frontend/app/shared/services/projects/api/update-project.ts
+++ frontend/app/shared/services/projects/api/update-project.ts
@@ -6,7 +6,7 @@ export const updateProject = async (id: number, data: Partial<Project>) => {
   const response = await fetchExtended<CustomResponse>(
     `${import.meta.env.VITE_BASE_PATH}/projects/${id}`,
     {
-      method: 'PUT',
+      method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
       },
